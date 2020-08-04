8b20381b81ca2e8d55c140baf9e3deeead5fde48
Shaban Karumba <shabankarumba@gmail.com>
2020-08-04 22:36:36 +0100

    Refactor tests

--- ruby/normal_item_rule_spec.rb
+++ ruby/normal_item_rule_spec.rb
@@ -42,109 +42,108 @@ describe NormalItemRule do
       let(:quality) { 10 }
 
       it 'does not change the quality' do
-        rule.set_quality
+        rule.apply
         expect(item.quality).to eq 9
       end
     end
-  end
 
-  describe '#set_sell_in' do
     it 'changes the sell in value' do
-      rule.set_sell_in
+      rule.apply
       expect(item.sell_in).to eq(-1)
     end
   end
 
-  describe ItemRule::AgedBrieItemRule do
-    describe '#set_quality' do
+  describe NormalItemRule::AgedBrieItemRule do
+    describe '#apply' do
       let(:quality) { 1 }
-      let(:rule) { ItemRule::AgedBrieItemRule.new(item) }
+      let(:rule) { NormalItemRule::AgedBrieItemRule.new(item) }
 
       it 'increases the quality by 1' do
-        rule.set_quality
-        expect(item.quality).to eq(2)
+        rule.apply
+        expect(item.quality).to eq(3)
       end
     end
   end
 
-  describe ItemRule::BackStagePassItemRule do
+  describe NormalItemRule::BackStagePassItemRule do
     let(:quality) { 5 }
     let(:item_name) { 'Backstage passes' }
-    subject(:rule) { ItemRule::BackStagePassItemRule.new(item) }
+    subject(:rule) { NormalItemRule::BackStagePassItemRule.new(item) }
 
-    context 'when the concert is less than 10 days away' do
-      let(:sell_in) { 10 }
+    describe '#appyly' do
+      context 'when the concert is less than 10 days away' do
+        let(:sell_in) { 10 }
 
-      it 'increases the the quality by 2' do
-        rule.set_quality
-        expect(item.quality).to eq(7)
+        it 'increases the the quality by 2' do
+          rule.apply
+          expect(item.quality).to eq(7)
+        end
       end
-    end
 
-    context 'when the concert is less than 10 days away' do
-      let(:sell_in) { 5 }
+      context 'when the concert is less than 10 days away' do
+        let(:sell_in) { 5 }
 
-      it 'increases the the quality by 3' do
-        rule.set_quality
-        expect(item.quality).to eq(8)
+        it 'increases the the quality by 3' do
+          rule.apply
+          expect(item.quality).to eq(8)
+        end
       end
-    end
 
-    context 'when the concert is less than 10 days away' do
-      let(:sell_in) { -1 }
+      context 'when the concert is less than 10 days away' do
+        let(:sell_in) { -1 }
 
-      it 'increases the the quality by 3' do
-        rule.set_quality
-        expect(item.quality).to eq(0)
+        it 'increases the the quality by 3' do
+          rule.apply
+          expect(item.quality).to eq(0)
+        end
       end
     end
   end
 
-  describe ItemRule::SulfurasItemRule do
+  describe NormalItemRule::SulfurasItemRule do
     let(:quality) { 80 }
     let(:item_name) { 'Backstage passes' }
     subject(:rule) { described_class.new(item) }
 
-    describe '#set_quality' do
+    describe '#apply' do
       it 'does not change the quality' do
-        rule.set_quality
+        rule.apply
         expect(item.quality).to eq 80
       end
-    end
 
-    describe '#sell_in' do
       it 'does not change the quality' do
-        rule.set_quality
+        rule.apply
         expect(item.sell_in).to eq 0
       end
     end
   end
 
-  describe ItemRule::ConjuredItemRule do
+  describe NormalItemRule::ConjuredItemRule do
     let(:quality) { 50 }
     let(:item_name) { 'Conjured' }
     subject(:rule) { described_class.new(item) }
 
-    context 'when the sell in has passed' do
-      let(:sell_in) { 0 }
+    describe '#apply' do
+      context 'when the sell in has passed' do
+        let(:sell_in) { 0 }
 
-      it 'quality degrades twice as fast' do
-        rule.set_quality
-        expect(item.quality).to eq 46
+        it 'quality degrades twice as fast' do
+          rule.apply
+          expect(item.quality).to eq 46
+        end
       end
-    end
 
-    context 'when the sell in has passed' do
-      let(:sell_in) { 1 }
+      context 'when the sell in has passed' do
+        let(:sell_in) { 1 }
 
-      it 'quality degrades twice as fast' do
-        rule.set_quality
-        expect(item.quality).to eq 48
+        it 'quality degrades twice as fast' do
+          rule.apply
+          expect(item.quality).to eq 48
+        end
       end
     end
   end
 end
 
 # Prevent quality going to less than 0
-# Add tests for GildedRose
 # Refactor
